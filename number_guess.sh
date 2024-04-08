6a148a091596406285d90b33bb4e1eb74ae1758f
dennis61820 <danngit@hotmail.com>
2024-04-08 19:09:38 +0000

    feat: added database

--- number_guess.sh
+++ number_guess.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-echo "i'm a game."
+
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+
+
+echo "Enter your username: "
+read USER_NAME
+
+
+# check if user name < 23 3characters
+LEN_NAME=${#USER_NAME}
+# echo $LEN_NAME
+if [[ ! $LEN_NAME > 22 ]]
+then
+  echo -e "\nPlease choose a name shorter than 23 characters:"
+  GET_USER
+fi
+
+echo "hello $USER_NAME"
